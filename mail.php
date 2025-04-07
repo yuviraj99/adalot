6ce9e1492b387a041be612d7d24958a08da5f47a
yuviraj99 <43059310+yuviraj99@users.noreply.github.com>
2025-04-07 09:44:58 +0530

    imp

--- mail.php
+++ mail.php
@@ -21,7 +21,7 @@
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "info@example.com";
+        $recipient = "adv.abhishek.legalservices@gmail.com";
 
         // Set the email subject.
         $subject = "New contact from $name";
